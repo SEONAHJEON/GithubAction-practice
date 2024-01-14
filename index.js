748ecbc2630c0d33b86aeef9ef291445d5cf536d
SEONAH <sajeon@gmail.com>
2024-01-14 18:51:14 -0400

    add sum and change test cases

--- index.js
+++ index.js
@@ -3,18 +3,10 @@ function factorial(n) {
   if (n < 0) {
     throw new Error('Factorial is only defined for non-negative integers!');
   } 
-  if (n === 0 || n === 1) {
+  if (n === 0) {
     return 1;
-  } else {
-    let factorial = 1;
-    for (let i = 2; i <= n; i++) {
-      factorial *= i;
-    }
-    return factorial;
   } 
-}
+  return n * factorial(n-1);
+  }
 
-//Example usage
-const inputNumber = 5;
-const result = factorial(inputNumber);
-console.log('The factorial of ${inputNumber} is: ${result}');
+module.exports = factorial;
